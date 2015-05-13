9b7d0d1ca5e63c312940f0193ce8e45dbbedcad7
jholkeboer <jholkeboer@users.noreply.github.com>
2015-05-13 12:53:44 -0700

    working on table on localhost

--- src/interface.php
+++ src/interface.php
@@ -1,16 +1,51 @@
+<!doctype html>
+<head>
+</head>
+<body>
 <?php
-	/*
-mysqli::__construct() (
-	[string $host = ini_get("mysqli.default_host") [, string $username =
-	ini_get("mysqli.default_user") [, string $passwd = 
-	ini_get("mysqli.default_pw") [, string $dbname = "blockbuster"]]]]
-)
-*/
-$mysqli = new mysqli("oniddb.cws.oregonstate.edu","holkeboj-db","VFGTEHMFAZYkn25Q","holkeboj-db");
+include 'storedinfo.php';
+
+//oregon state db
+
+//$mysqli = new mysqli("oniddb.cws.oregonstate.edu","holkeboj-db",$holkebojpass,"holkeboj-db");
+//if(!$mysqli || $mysqli->connect_errno) {
+//	echo "Unable to connect to database.  Error: " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+//} else {
+//	echo("connected");
+//}
+
+
+//local db
+$mysqli = new mysqli("localhost","root",$localpass,"blockbuster","3306");
 if(!$mysqli || $mysqli->connect_errno) {
 	echo "Unable to connect to database.  Error: " . $mysqli->connect_errno . " " . $mysqli->connect_error;
 } else {
 	echo("connected");
 }
 
+//general statement for getting all videos in db
+if (!($getVids = $mysqli->prepare("SELECT id, name, category, length, rented FROM vidstore ORDER BY name"))) {
+	echo "Prepare failed on getVids";	
+}
+if (!$getVids->execute()) {
+	echo "Execute failed on getVids";
+}
+$vidResult = $getVids->get_result();
 ?>
+<table>
+<?php
+while($row = $vidResult->fetch_assoc()) {
+	foreach($row as $r) {
+		echo $row['name'];
+	}
+}
+?>
+</table>
+<?php
+
+
+$getVids->close();
+
+$mysqli->close();
+?>
+</body>
--- src/schema.sql
+++ src/schema.sql
@@ -3,13 +3,39 @@ create table vidstore (
 	id int(11) NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
 	category varchar(255),
-	length int(11),
-	rented bool,
+	length int(11) unsigned,
+	rented bool default 1,
 	PRIMARY KEY (id),
 	UNIQUE(name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*
-how to initialize bool to 1?
-how to make name required? (besines not null)
-how to make sure length is positive?
+
 */
+INSERT INTO vidstore (name, category, length) VALUES
+('Avengers: Age of Ultron', 'Action', 141),
+('Furious Seven', 'Action', 137),
+('Mad Max: Fury Road', 'Action', 120),
+('The Avengers', 'Action', 143),
+('Jupiter Ascending', 'Action', 127),
+('Kingsman: The Secret Service', 'Comedy', 129),
+('Mortdecai', 'Comedy', 107),
+('Paul Blart: Mall Cop 2', 'Comedy', 94),
+('The Wedding Ringer', 'Comedy', 101),
+('Home', 'Comedy', 94),
+('The Age of Adaline', 'Drama', 112),
+('Ex Machina', 'Drama', 108),
+('The Water Diviner', 'Drama', 111),
+('Fifty Shades of Gray', 'Drama', 125),
+('The Longest Ride', 'Drama', 139);
+
+--prepared statements below
+
+--add video
+
+--check in video
+
+--check out video
+
+--delete all videos
+
+--get all movies in given category
